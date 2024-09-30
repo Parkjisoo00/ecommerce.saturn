065261ed016237a70495bc577f63c9404f025dc2
진유혁 <cydgate4957@gmail.com>
2024-09-30 14:20:54 +0900

    [INSERT] Template

---
+++ src/kr/co/bravomylife/common/dto/FileUploadDto.java
@@ -0,0 +1,26 @@
+package kr.co.bravomylife.common.dto;
+
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @version 1.0.0
+ * @author cydgate4957@gmail.com
+ * 
+ * @since 2024-09-30
+ * <p>DESCRIPTION:</p>
+ * <p>IMPORTANT:</p>
+ */
+public class FileUploadDto {
+	
+	private List<MultipartFile> files;
+	
+	
+	public List<MultipartFile> getFiles() {
+		return files;
+	}
+	public void setFiles(List<MultipartFile> files) {
+		this.files = files;
+	}
+}
