3dd1bc6d4db8c8b90f2b5d6fd6ca072280444ea5
김민선 <alstjsrla28@gmail.com>
2024-10-02 12:41:57 +0900

    [UPDATE]

--- WebContent/js/front.js
+++ WebContent/js/front.js
@@ -143,7 +143,17 @@ function checkRegister() {
 		alert("연락처를 확인해 주세요!");
 		isSubmit = false;
 	}
-		
+	
+	var birth = new Date(document.getElementById("age").value);
+	var today = new Date();
+	var age = today.getFullYear() - birth.getFullYear();
+
+	// 생일이 지나지 않았으면 나이를 1살 줄임
+	if (today.getMonth() < birth.getMonth() || 
+		(today.getMonth() === birth.getMonth() && today.getDate() < birth.getDate())) {
+		age--;
+	}
+			
 	var age = document.getElementById("age").value;
 
 	if (document.getElementById("mbr_nm").value == ""
